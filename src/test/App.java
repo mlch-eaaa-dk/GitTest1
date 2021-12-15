bea346e2a4cd25f02a444596cabf07c1496560b5
MLCH <mlch@eaaa.dk>
2021-12-15 13:42:22 +0100

    Changed Person class

--- src/test/App.java
+++ src/test/App.java
@@ -5,13 +5,13 @@ import model.Person;
 public class App {
 
     public static void main(String[] args) {
-        Person per = new Person("Per");
-        System.out.println(per.getName());
+        Person per = new Person("Per", 20);
+        System.out.println(per);
 
-        Person ulla = new Person("Ulla");
-        System.out.println(ulla.getName());
+        Person ulla = new Person("Ulla", 25);
+        System.out.println(ulla);
 
-        Person jens = new Person("Jens");
-        System.out.println(ulla.getName());
+        Person jens = new Person("Jens", 22);
+        System.out.println(ulla);
     }
 }
