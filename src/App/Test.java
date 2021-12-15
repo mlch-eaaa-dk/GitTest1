18f8d992703d62457c3ed7ff96dc6ccbff0e2551
MLCH <mlch@eaaa.dk>
2021-12-15 13:28:57 +0100

    Added and changed

--- src/App/Test.java
+++ src/App/Test.java
@@ -5,10 +5,13 @@ import model.Person;
 public class Test {
 
     public static void main(String[] args) {
-        Person ib = new Person("Ib");
-        System.out.println(ib.getName());
+        Person per = new Person("Per");
+        System.out.println(per.getName());
 
         Person ulla = new Person("Ulla");
+        System.out.println(ulla.getName());
+
+        Person jens = new Person("Jens");
         System.out.println(ulla.getName());
     }
 }
