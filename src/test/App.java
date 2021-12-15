e44bdbedb910cf3e80b3ef34fcf5fa3dd2064ce3
MLCH <mlch@eaaa.dk>
2021-12-15 13:47:10 +0100

    Readded one persons in App

--- src/test/App.java
+++ src/test/App.java
@@ -7,5 +7,8 @@ public class App {
     public static void main(String[] args) {
         Person per = new Person("Per", 20);
         System.out.println(per);
+
+        Person ulla = new Person("Ulla", 25);
+        System.out.println(ulla);
     }
 }
