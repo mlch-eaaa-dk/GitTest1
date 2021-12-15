02d59ddfec84b08fb956072a3a5aebe8ca383126
MLCH <mlch@eaaa.dk>
2021-12-15 13:45:32 +0100

    Deleted persons in App

--- src/test/App.java
+++ src/test/App.java
@@ -7,11 +7,5 @@ public class App {
     public static void main(String[] args) {
         Person per = new Person("Per", 20);
         System.out.println(per);
-
-        Person ulla = new Person("Ulla", 25);
-        System.out.println(ulla);
-
-        Person jens = new Person("Jens", 22);
-        System.out.println(ulla);
     }
 }
