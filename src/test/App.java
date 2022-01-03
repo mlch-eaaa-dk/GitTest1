50247eac4b7b897c5c7fedd260ea5ecc8c423a9e
MLCH <mlch@eaaa.dk>
2022-01-03 11:19:38 +0100

    Deleted 3 persons in App

--- src/test/App.java
+++ src/test/App.java
@@ -8,10 +8,5 @@ public class App {
         Person per = new Person("Per", 20);
         System.out.println(per);
 
-        Person ulla = new Person("Ulla", 25);
-        System.out.println(ulla);
-
-        Person pia = new Person("Pia", 30);
-        System.out.println(pia);
     }
 }
