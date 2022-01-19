3f7b62a04ccfd0d76a81ce07f1e2fd7fcdef1bcd
MLCH <mlch@eaaa.dk>
2022-01-19 18:25:42 +0100

    Changed a preference

--- src/test/App.java
+++ src/test/App.java
@@ -7,6 +7,7 @@ public class App {
     public static void main(String[] args) {
         Person per = new Person("Per", 20);
         System.out.println(per);
+        System.out.println(per);
 
     }
 }
